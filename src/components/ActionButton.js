d4c4d57877b19631f22d5150fa59c5e9749bfd98
Andjela Babic <andjela.babic@vanderlande.com>
2020-05-09 23:27:14 +0200

    Extracted component OpenForm

--- src/components/ActionButton.js
+++ src/components/ActionButton.js
@@ -1,11 +1,11 @@
 import React from "react"; 
-import AddIcon from "@material-ui/icons/Add";
 import CloseIcon from '@material-ui/icons/Close';
 import { Card, Button, Icon } from "@material-ui/core";
 import TextArea from 'react-textarea-autosize'; 
 import { connect } from "react-redux"; 
 import { addList, addCard } from "../actions"; 
 import styled from "styled-components";
+import OpenForm from "./OpenForm";
 
 class ActionButton extends React.Component {
     state = {
@@ -105,33 +105,9 @@ class ActionButton extends React.Component {
 
     renderAddButton = () => {
         const { list } = this.props; 
-
         const buttonText = list ? "Add another list" : "Add another card"; 
-        const buttonTextOpacity = list ? 1 : 0.5; 
-        const buttonTextColor = list ? "white" : "inherit"; 
-        const buttonTextBackground = list ? "rgba(0,0,0,.15)" : "inherit"; 
-
-        const OpenFormButton = styled.div`
-        display: flex;
-        align-items: center;
-        cursor: pointer;
-        border-radius: 3px;
-        height: 36px;
-        margin-left: 8px;
-        width: 300px;
-        padding-left: 10px;
-        padding-right: 10px;
-        opacity: ${buttonTextOpacity};
-        color: ${buttonTextColor};
-        background-color: ${buttonTextBackground};
-        `;
-
         return (
-            <OpenFormButton
-            onClick={this.openForm} >   
-                <AddIcon></AddIcon>
-                <p style={{ flexShrink: 0 }}>{buttonText}</p>
-            </OpenFormButton>
+            <OpenForm list={list} children={buttonText} openForm={this.openForm}></OpenForm>
         )
     }
 
